4df25ec49a3444f4cdcf731f21c2336bd2b0f35f
Dmytro Babych <99317239+baby4dm@users.noreply.github.com>
2026-06-19 16:38:08 +0300

    starting project

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+export default function Quiz() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+}
